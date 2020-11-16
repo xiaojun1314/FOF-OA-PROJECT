2d609221d4405d76b883acb2d0887aceb9d9ac92
xiaojun1314 <664922906@qq.com>
2020-11-16 16:00:15 +0800

    测试信息1

--- src/main/java/com/fof/App.java
+++ src/main/java/com/fof/App.java
@@ -8,8 +8,6 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        System.out.println("测试代码");
-        System.out.println("测试代码");
     }
 
 }
