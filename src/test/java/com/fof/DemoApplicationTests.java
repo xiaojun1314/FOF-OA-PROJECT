83dee6d0774bfb34f360b0ab7e5f14dd1cf2da3c
xiaojun1314 <664922906@qq.com>
2020-11-16 15:54:14 +0800

    Initial commit

---
+++ src/test/java/com/fof/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
